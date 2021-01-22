d9f4bebeaee09b2cf77785fd347a7504e0e1abe7
yw <ywang035@gmail.com>
2021-01-22 21:55:58 +1100

    2nd commit
    add check box to task
    can swap between different lists

--- app.js
+++ app.js
@@ -1,71 +1,86 @@
-// delete tasks
-// attach button with even - event bubble way
-const list = document.querySelector("#current-task-list ul");
+const lists = document.querySelectorAll(".task-list ul");
+Array.from(lists).forEach(myFunction);
 
-list.addEventListener('click', function(e){
-    if(e.target.className == 'delete'){
-        const li = e.target.parentElement;
-        li.parentNode.removeChild(li);
-    }
-});
+function myFunction(list) {
+    list.addEventListener('click', function (e) {
+        console.log(e.target);
+        if (e.target.className == 'delete') {
+            const li = e.target.parentElement;
+            li.parentNode.removeChild(li);
+        }
 
+        if (e.target.className == 'check-box') {
+            const strikeTextBox = e.target;
+            const taskText = e.target.nextElementSibling;
+            if (strikeTextBox.checked) {
+                taskText.classList.add('task-finished');
+                const thisLi = e.target.parentElement;
+                const thisUl = thisLi.parentElement;
+                const finishedList = document.querySelector("#finished-task-list ul");
+                thisUl.removeChild(thisLi);
+                finishedList.append(thisLi);
+            } else {
+                taskText.classList.remove('task-finished');
+                const thisLi = e.target.parentElement;
+                const thisUl = thisLi.parentElement;
+                const currentList = document.querySelector("#current-task-list ul");
+                thisUl.removeChild(thisLi);
+                currentList.append(thisLi);
+            }
+        }
+    });
+}
 
 
 // add to current-task-list from form
 const addForm = document.forms['add-task']; // 'add-task' is id
-
-addForm.addEventListener('submit', function(e){
+const currentList = document.querySelector("#current-task-list ul");
+addForm.addEventListener('submit', function (e) {
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
 
-    // creat element
-    const li = document.createElement('li');
-    const taskName = document.createElement('span');
-    const deleteButton = document.createElement('span');
+    if (value.length > 0) {
+        // creat element
+        const li = document.createElement('li');
+        const checkBox = document.createElement('input');
+        const taskName = document.createElement('span');
+        const deleteButton = document.createElement('span');
 
-    // add content
-    deleteButton.textContent = 'delete';
-    taskName.textContent = value;
+        // add content
+        deleteButton.textContent = 'delete';
+        taskName.textContent = value;
+        checkBox.setAttribute('type', 'checkbox');
 
-    // add style
-    deleteButton.classList.add("delete");
-    taskName.classList.add('task');
+        // add style
+        deleteButton.classList.add("delete");
+        taskName.classList.add('task');
+        checkBox.classList.add('check-box');
 
-    // append to list
-    li.appendChild(taskName);
-    li.appendChild(deleteButton);
-    list.appendChild(li);
+        // append to list
+        li.appendChild(checkBox);
+        li.appendChild(taskName);
+        li.appendChild(deleteButton);
+        currentList.prepend(li);
+
+        addForm.querySelector('input[type="text"]').value = ""
+    }
 });
-
-
-
-// hide tasks
-// const hideBox = document.querySelector('#hide');
-// hideBox.addEventListener('change', function(e){
-
-//     if(hideBox.checked){
-//         list.style.display = "none";
-//     }else{
-//         list.style.display = "initial";
-//     }
-// });
-
 
 
 // search tasks
 const searchBar = document.forms['search-tasks'].querySelector('input');
 
-searchBar.addEventListener('keyup', function(e){
+searchBar.addEventListener('keyup', function (e) {
     const term = e.target.value.toLowerCase();
     const tasks = list.getElementsByTagName('li');
 
-    Array.from(tasks).forEach(function(task){
+    Array.from(tasks).forEach(function (task) {
 
         const taskText = task.firstElementChild.textContent;
 
-        if(taskText.toLowerCase().indexOf(term) != -1){
+        if (taskText.toLowerCase().indexOf(term) != -1) {
             task.style.display = 'flex';
-        }else{
+        } else {
             task.style.display = 'none';
         }
     });
